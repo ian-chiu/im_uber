1235cebaa1967a0a7d1a220babc5b46623a07c16
Terry Chan <terrychan999@gmail.com>
2023-05-29 01:16:34 +0800

    add users and auth method

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StopsController } from './stops/stops.controller';
 import { StopsService } from './stops/stops.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController, StopsController],
   providers: [AppService, StopsService],
 })
