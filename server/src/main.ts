dabf4c0ff126030590a1b78b796ae1055339e55e
Terry Chan <terrychan999@gmail.com>
2023-06-04 15:50:56 +0800

    enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,7 @@ import * as passport from 'passport';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug'],
+    cors: true,
   });
 
   app.use(
